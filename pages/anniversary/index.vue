6586d4d7f5ab51088b29ce893b67dcf628aa0db9
TC Wang <wangtct@hotmail.com>
2024-10-21 16:49:38 +0800

    1021-1640

--- pages/anniversary/index.vue
+++ pages/anniversary/index.vue
@@ -2,10 +2,10 @@
   <div
     class="container w-full max-w-full columns-1 md:columns-2 prose lg:prose-base prose-h2:my-1 prose-h2:text-lg prose-h3:my-1 prose-h3:text-base prose-img:w-full"
   >
-    <h2 class="">1964 台北市女中 重聚會 - 畢業60週年紀念</h2>
+    <h2 class="">1964 台北市立女中 重聚會 - 畢業60週年紀念</h2>
 
     <p class="pt-4">
-      2024年10月16日台北市女中同學，在光輝的十月，舉國歡騰的日子，同時也舉辦了一塲畢業60周年重聚日。
+      2024年10月16日台北市女中同學，在光輝的十月，舉國歡騰的日子，同時也舉辦了一塲畢業60周年重聚會。
     </p>
     <p>
       少女情懷總是詩。初中時期，人生最快樂無憂的日子。我們曾經有幸相逢相識，在新生南路二段32號市立女中校園，度過三年美好的歲月。憶往想起我們曾經在校園𥚃的荷花池跳上跳下，也曾經在防空洞上嬉戲。
@@ -21,6 +21,24 @@
     </p>
 
     <img src="/images/IMG8/學校.jpg" class="img-fluid" style="width: 400px" />
+
+    <h2>我們的師長</h2>
+    <h3>3班 張炳煜導師 - 於2022年</h3>
+    <img src="/images/IMG8/S_201.jpg" class="img-fluid" style="width: 400px" />
+    <img src="/images/IMG8/S_202.jpg" class="img-fluid" style="width: 400px" />
+
+    <h3>9班 楊鴻秋導師 - 演講內容</h3>
+
+    <img src="/images/IMG8/S_301.jpg" class="img-fluid" style="width: 400px" />
+
+    <h2>12班 重返校園</h2>
+
+    <img src="/images/IMG8/S_101.jpg" class="img-fluid" style="width: 600px" />
+    <img src="/images/IMG8/S_102.jpg" class="img-fluid" style="width: 600px" />
+
+    <img src="/images/IMG8/S_104.jpg" class="img-fluid" style="width: 600px" />
+    <img src="/images/IMG8/S_105.jpg" class="img-fluid" style="width: 600px" />
+    <img src="/images/IMG8/S_103.jpg" class="img-fluid" style="width: 400px" />
   </div>
 </template>
 
