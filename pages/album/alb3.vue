a4f99aa84ac72ee5260ab8a8af8b9103cb02ebf9
TC Wang <wangtct@hotmail.com>
2024-10-24 09:32:06 +0800

    1024-0931

--- pages/album/alb3.vue
+++ pages/album/alb3.vue
@@ -3,13 +3,14 @@
     class="container w-full max-w-full columns-1 md:columns-2 prose lg:prose-base prose-h2:my-2 prose-h2:text-lg prose-h3:mb-2 pb-2 prose-h3:text-base prose-img:w-full"
   >
     <h2>1964市女中 - 畢業電子相冊</h2>
-    請點選 相片中的左右鍵 輪播相片
 
     <h3>1-10 班相冊</h3>
+    註: <scope class="text-red-600">請點選 下面的左右鍵 輪播相片</scope>
     <AlbumCarousel1 />
 
     <br />
     <h3>11 - 17 班相冊</h3>
+    註: <scope class="text-red-600">請點選 下面的左右鍵 輪播相片</scope>
     <AlbumCarousel2 />
   </div>
 </template>
