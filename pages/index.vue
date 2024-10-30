95f6c3b2abf83df8f9caa053930f481191ee3bd4
TC Wang <wangtct@hotmail.com>
2024-10-31 07:32:51 +0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
+  <div
+    class="container w-full max-w-full columns-1 md:columns-2 prose lg:prose-base prose-h2:my-1 prose-h2:text-lg prose-h3:my-1 prose-h3:text-base prose-img:w-full"
+  >
     <h2>台北市立女中 - 1964級同學會</h2>
 
-    <img src="/images/IMG9/S_008.jpg" class="img-fluid w-full md:w-1/2" />
+    <h3>目的</h3>
+    <p>
+      本網站主要在提供同學未參加
+      或參加1964年市女中畢業60年重聚會者，觀看現場實況，
+      包括拍攝的照片及錄影內容； 同時提供畢業紀念册各班級
+      導師及同學的照片。期能提供同學們回憶以往，並期待未來 作相互聯絡使用。
+    </p>
 
+    <h3>內容</h3>
+    <p>
+      本網站的內容涵蓋2024年10月16日在大龍蝦餐廳所舉辦1964年市女中畢業60年重聚會拍攝的照片及表演節目錄影。
+      計有259張照片及19部影片。另外，本網站也將1964年市
+      女中畢業紀念册翻拍電子相册。 供同學查閱使用 。
+    </p>
+
+    <h3>使用方法</h3>
+    <nl>
+      <li>
+        QR Cod
+        協助提供使用者，利用手機，對準QR碼拍攝，就可快速取得並且讀取資訊內容。
+      </li>
+      <li>
+        使用者在看任何頁面時， 只要點擊 看到網頁最上面
+        左邊的黃色「市女中」就會回到首頁。
+      </li>
+      <li>
+        手機使用者點擊右上角「=」符號，卽可打開網頁，看到網站綱目，點選想要看到的項目，即可看到內容。
+      </li>
+    </nl>
     <br />
-
     本網站QR Code: 請掃描或點擊進入此網站
     <a href="https://alumni-tmghs.netlify.app">
       <img
@@ -14,6 +42,8 @@
         style="width: 150px"
       />
     </a>
+
+    <img src="/images/IMG9/S_008.jpg" class="img-fluid" />
 
     <br />
   </div>
