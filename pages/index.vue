02c5f467980ce8ce39a7b219e42111d01e36112d
TC Wang <wangtct@hotmail.com>
2024-10-30 09:29:30 +0800

    qrcode

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
     <br />
 
     本網站QR Code: 請掃描或點擊進入此網站
-    <a href="https://alumni-tmghs.netlify.app/anniversary/ann1">
+    <a href="https://alumni-tmghs.netlify.app">
       <img
         src="/images/IMG8/qrcode.png"
         class="img-fluid"
