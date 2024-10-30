c722b001f43a415da278847b01a218754c7aad00
TC Wang <wangtct@hotmail.com>
2024-10-30 09:18:34 +0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,10 @@
   <div>
     <h2>台北市立女中 - 1964級同學會</h2>
 
+    <img src="/images/IMG9/S_008.jpg" class="img-fluid w-full md:w-1/2" />
+
+    <br />
+
     本網站QR Code: 請掃描或點擊進入此網站
     <a href="https://alumni-tmghs.netlify.app/anniversary/ann1">
       <img
@@ -10,6 +14,7 @@
         style="width: 150px"
       />
     </a>
+
     <br />
   </div>
 </template>
